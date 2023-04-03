17a197a9268ea93d317e57ae639ca8c8ac394534
0-th <artemisfowlx@protonmail.com>
2023-04-03 17:19:41 +0100

    fix: correctly add user model to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,5 +10,8 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'username', 'name', 'is_staff']
-    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('name',)}))
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('name',)}))
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('name',)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('name',)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
