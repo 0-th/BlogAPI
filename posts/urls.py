53b2601ed5aefbe011368d80a5e76ee129809c66
0-th <artemisfowlx@protonmail.com>
2023-04-06 14:44:41 +0100

    refactor: organize views and urlconfs better

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import PostList, PostDetail, UserList, UserDetail
+from .views import PostViewSet, UserViewSet
 
-urlpatterns = [
-    path('users/<int:pk>/', UserDetail.as_view()),
-    path('users/', UserList.as_view()),
-    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
-    path('', PostList.as_view(), name='post_list'),
-]
+router = SimpleRouter()
+router.register(prefix='users', viewset=UserViewSet, basename='users')
+router.register(prefix='', viewset=PostViewSet, basename='posts')
+
+urlpatterns = router.urls
